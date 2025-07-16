08ab4d2b5c879d9a9b21c9695a7bf995519c6225
Matt Brown <matt@elyos.ai>
2025-07-16 12:13:35 +0100

    Re add shutdown_on_listener_failure

--- pgqueuer/applications.py
+++ pgqueuer/applications.py
@@ -73,6 +73,8 @@ class PgQueuer:
         dequeue_timeout: timedelta = timedelta(seconds=30),
         batch_size: int = 10,
         mode: QueueExecutionMode = QueueExecutionMode.continuous,
+        max_concurrent_tasks: int | None = None,
+        shutdown_on_listener_failure: bool = False,
     ) -> None:
         """
         Run both QueueManager and SchedulerManager concurrently.
@@ -91,6 +93,8 @@ class PgQueuer:
                         batch_size=batch_size,
                         dequeue_timeout=dequeue_timeout,
                         mode=mode,
+                        max_concurrent_tasks=max_concurrent_tasks,
+                        shutdown_on_listener_failure=shutdown_on_listener_failure,
                     )
                 )
             )
